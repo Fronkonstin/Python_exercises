a7378b804a819c29a9b7aa000a51ae127191f023
Fronkonstin <105315051+Fronkonstin@users.noreply.github.com>
2024-03-07 16:59:52 +0100

    Final changes.

---
+++ Hangman/words.py
@@ -0,0 +1,19 @@
+#Main Program
+
+words = ['rainbow', 'computer', 'science', 'programming', 'python', 'mathematics', 'player', 'condition', 'reverse', 'water', 'board']
+
+'''
+letters = []
+
+while True:
+    user_input = input('Introduce una letra (o "0" para salir): ')
+    
+    if user_input == '0':
+        break
+    
+    letters.append(user_input)
+    print('Lista de letras ingresadas:', letters)
+
+print('Programa finalizado.')
+'''
+
